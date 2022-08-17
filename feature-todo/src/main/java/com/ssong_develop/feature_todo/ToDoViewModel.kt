586297e235f42707824775fd994b609907b59d57
SSong-develop <thdgnsrl123@naver.com>
2022-08-18 00:06:23 +0900

    checkbox Single selection

--- feature-todo/src/main/java/com/ssong_develop/feature_todo/ToDoViewModel.kt
+++ feature-todo/src/main/java/com/ssong_develop/feature_todo/ToDoViewModel.kt
@@ -1,14 +1,30 @@
 package com.ssong_develop.feature_todo
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.ssong_develop.core_data.repository.TodoRepository
+import com.ssong_develop.model.Todo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
+// fixme ssong-develop
+// 어떻게 하면 CheckBox Single Selection이 가능할까 생각해봐야 할듯
 @HiltViewModel
 class ToDoViewModel @Inject constructor(
     private val todoRepository: TodoRepository
 ) : ViewModel() {
 
     val todos = todoRepository.getTodosStream()
+
+    val fakeItemList = flowOf(
+        listOf<Todo>(
+            Todo("1", "example1", "ex", "123123"),
+            Todo("2", "example2", "ex", "123123"),
+            Todo("3", "example3", "ex", "123123"),
+            Todo("4", "example4", "ex", "123123"),
+            Todo("5", "example5", "ex", "123123")
+        )
+    )
 }
--- feature-todo/src/main/java/com/ssong_develop/feature_todo/TodoScreen.kt
+++ feature-todo/src/main/java/com/ssong_develop/feature_todo/TodoScreen.kt
@@ -1,59 +1,89 @@
 package com.ssong_develop.feature_todo
 
-import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.coroutineScope
 
 @Composable
 fun TodoScreen(
     modifier: Modifier = Modifier,
     viewModel: ToDoViewModel = hiltViewModel()
 ) {
-    val todos by viewModel.todos.collectAsState(initial = emptyList())
+    // Ui Data
+    val listState = rememberLazyListState()
+    var selectedPosition by remember { mutableStateOf(-1) }
 
-    if (todos.isEmpty()) {
-        Column(
-            modifier = modifier
-                .fillMaxSize()
-                .padding(20.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text("Todo를 등록해주세요.")
-        }
-    } else {
-        LazyColumn(
-            content = {
-                items(todos.size) { index ->
-                    Card(
-                        modifier = modifier
-                            .fillMaxSize()
-                            .padding(20.dp),
-                        shape = RoundedCornerShape(8.dp),
-                        elevation = 6.dp
-                    ) {
-                        Column(
-                            modifier = modifier
-                                .fillMaxWidth()
-                        ) {
-                            Text(todos[index].todoTitle)
+    // Business Data
+    val todos by viewModel.fakeItemList.collectAsState(initial = emptyList())
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar() {
+                Text("할 일")
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { },
+                elevation = FloatingActionButtonDefaults.elevation(8.dp),
+                content = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_trash_can),
+                        contentDescription = ""
+                    )
+                }
+            )
+        },
+        content = { padding ->
+            if (todos.isEmpty()) {
+                Column(
+                    modifier = modifier
+                        .fillMaxSize()
+                        .padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text("Todo를 등록해주세요.")
+                }
+            } else {
+                LazyColumn(
+                    state = listState,
+                    content = {
+                        items(todos.size) { index ->
+                            Card(
+                                modifier = modifier
+                                    .fillMaxSize()
+                                    .padding(20.dp),
+                                shape = RoundedCornerShape(8.dp),
+                                elevation = 6.dp
+                            ) {
+                                Row(
+                                    modifier = modifier
+                                        .fillMaxWidth()
+                                ) {
+                                    Checkbox(
+                                        checked = selectedPosition == index,
+                                        onCheckedChange = { _ ->
+                                            selectedPosition = index
+                                        }
+                                    )
+                                    Text(todos[index].todoTitle)
+                                }
+                            }
                         }
                     }
-                }
+                )
             }
-        )
-    }
-
+        }
+    )
 }
